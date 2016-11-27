e39fee473762bfd8c289246b39ea213f7cf52877
Komal Sharan <komalssharan@gmail.com>
2016-11-26 20:25:57 -0500

    Update CurrentBestHypothesis.py

--- CurrentBestHypothesis.py
+++ CurrentBestHypothesis.py
@@ -1,40 +1,82 @@
-constraints=[]
-currentBestHypothesis=[]
-forvalidation=[]
 
-def probability(x):
-  return x;
+from new_eleusis import *
+from scientist import *
+from card_generator import *
 
 
-'''
-This function returns the list of rules which have to be tested. 
-'''
-def validator(x):
- forvalidation.append(x)
- return list
 
-def check_for_upper_threshold(x):
- if probability(x) > 0.5:
-  return True
- else:
-   return False
+constraints={}
+currentBestHypothesis={}
+forvalidation={}
+threshold=0.0
+count=1
 
-def check_for_lower_threshhold(x):
- if probability(x) < 0.2:
-  return True
- else:
-  return False
 
 def current_best_hypothesis(hypothesis):
-  for x in hypothesis:
-   if check_for_upper_threshold(x):
-    currentBestHypothesis.append(hypothesis)
+  temp={}
+  tempconstraints={}
+  tempValidation={}
+  for (x,y) in hypothesis:
+   for a in y:
 
-   if check_for_lower_threshhold(x):
-     constraints.append(x)
+    if  hypothesis[x][a]>0.005:
+     temp[y]=hypothesis[x][a]
 
-   else:
-     validator(x)
+
+    if hypothesis[x][a]<0.001:
+      tempconstraints[y] =hypothesis[x][a]
+
+
+    else:
+      tempValidation[y]=hypothesis[x][a]
+   currentBestHypothesis[x]=temp
+   constraints[x]=tempconstraints
+   forvalidation[x]=tempValidation
 
    return currentBestHypothesis
+
+
+def main():
+ for i in range(1,200):
+    previous2 = extraction(l[0])
+    previous1 = extraction(l[1])
+    current = extraction(l[2])
+    comparison = comparator(previous2, previous1, current)
+    p = probability(previous2, previous1, current, comparison)
+    # print "for first three cards"
+    # print comparison
+    # print p
+    count=i
+    card = card_generator()
+    print card + '\n'
+    correct_wrong(card)
+    card1 = extraction(card)
+    previous2 = previous1
+    previous1 = current
+    current = card1
+    comparison1 = comparator(previous2, previous1, current)
+    p1 = probability(previous2, previous1, current, comparison)
+    a1 = alternate(previous2, previous1, current, comparison)
+    current_best_hypothesis(p1)
+
+    print "for next card" + '\n'
+    print comparison1
+    print p1
+    print a1
+
+
+ if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
 
