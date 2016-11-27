e1def6446a5e4d4ba27157f4fe8afa0f3dc0dac4
Komal Sharan <komalssharan@gmail.com>
2016-11-27 02:54:26 -0500

    needs changes for alternating sequence

--- CurrentBestHypothesis.py
+++ CurrentBestHypothesis.py
@@ -12,32 +12,44 @@ threshold=0.0
 count=1
 
 
+"""def alternate(a1t):
+ for field, possible_values in hypothesis.iteritems():
+  print  field, possible_values
+   for a, x in possible_values.iteritems():"""
+
+
+
+
+
 def current_best_hypothesis(hypothesis):
   temp={}
   tempconstraints={}
   tempValidation={}
-  for (x,y) in hypothesis:
-   for a in y:
-
-    if  hypothesis[x][a]>0.005:
-     temp[y]=hypothesis[x][a]
 
 
-    if hypothesis[x][a]<0.001:
-      tempconstraints[y] =hypothesis[x][a]
-
-
+  for field, possible_values in hypothesis.iteritems():
+   print  field , possible_values
+   for a,x in possible_values.iteritems():
+    if x>0.005:
+     temp[a]=x
+    if x<0.002:
+     tempconstraints[a]=x
     else:
-      tempValidation[y]=hypothesis[x][a]
-   currentBestHypothesis[x]=temp
-   constraints[x]=tempconstraints
-   forvalidation[x]=tempValidation
+      tempValidation[a]=x
+    currentBestHypothesis[field] = temp
+    constraints[field] = tempconstraints
+    forvalidation[field] = tempValidation
 
-   return currentBestHypothesis
+   print "THIS" ,currentBestHypothesis
+
+
+
+
+  return currentBestHypothesis
 
 
 def main():
- for i in range(1,200):
+
     previous2 = extraction(l[0])
     previous1 = extraction(l[1])
     current = extraction(l[2])
@@ -46,9 +58,9 @@ def main():
     # print "for first three cards"
     # print comparison
     # print p
-    count=i
+
     card = card_generator()
-    print card + '\n'
+
     correct_wrong(card)
     card1 = extraction(card)
     previous2 = previous1
@@ -57,18 +69,18 @@ def main():
     comparison1 = comparator(previous2, previous1, current)
     p1 = probability(previous2, previous1, current, comparison)
     a1 = alternate(previous2, previous1, current, comparison)
-    current_best_hypothesis(p1)
 
-    print "for next card" + '\n'
+    current_best_hypothesis(p1)
+#    alternate(a1)
+
+    print "for next card " + '\n'
     print comparison1
     print p1
-    print a1
+    print "ALTERNATE" ,a1
 
 
- if __name__ == "__main__":
-    main()
-
-
+if __name__ == "__main__":
+ main()
 
 
 
