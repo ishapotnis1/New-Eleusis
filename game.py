89b6395bdb70ac19c24dac88698488d966247d0c
neetikakhurana <neetikakhurana@gmail.com>
2016-12-11 02:00:08 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -1,126 +1,33 @@
 #Put your program name in place of program_name
 
-from eleusis import *
+#from program_name import *
 from random import randint
 from new_eleusis import *
-import re
-
-from scientist import *
-
+from rule import *
+from prune import *
 global game_ended
 game_ended = False
+
+def set_rule(rule):
+    return rule
 
 def generate_random_card():
     values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     suits = ["S", "H", "D", "C"]
     return values[randint(0, len(values)-1)] + suits[randint(0, len(suits)-1)]
 
-hypo = "orf(even(current),equal(color(current),R),greater(prev,current))"
 class Player(object):
-
     def __init__(self):
-        #self.card=[]
-        self.hand = [generate_random_card() for i in range(0,14)]
-        self.card = []
-        '''
-        self.card[0] = extraction(self.hand[0])
-        self.card[1] = extraction(self.hand[1])
-        self.card[2] = extraction(self.hand[2])
-        self.card[3] = extraction(self.hand[3])
-        self.card[4] = extraction(self.hand[4])
-        self.card[5] = extraction(self.hand[5])
-        self.card[6] = extraction(self.hand[6])
-        self.card[7] = extraction(self.hand[7])
-        self.card[8] = extraction(self.hand[8])
-        self.card[9] = extraction(self.hand[9])
-        self.card[10] = extraction(self.hand[10])
-        self.card[11] = extraction(self.hand[11])
-        self.card[12] = extraction(self.hand[12])
-        self.card[13] = extraction(self.hand[13])
-        '''
+        self.hand = [generate_random_card() for i in range(14)]
 
-    def play(self, cards):
+    def play(self, hypothesis,cards):
         """
         'cards' is a list of three valid cards to be given by the dealer at the beginning of the game.
         Your scientist should play a card out of its given hand OR return a rule, not both.
         'game_ended' parameter is a flag that is set to True once the game ends. It is False by default
         """
-        card_dict = []
-        current_hand = [self.hand[0], cards[2], cards[1]]
-        current  = self.hand[0]
-        previous = cards[2] #replace with card from corrent list later
-        previous2 = cards[1]
-        data = "orf(even(current),equal(color(current),R),greater(prev,current)) "
-        if 'orf' in data:
-            data = data.split('orf(')[1]
-            data = data.rsplit(')', 1)[0]
-            #data = data.split("),")
-            if 'even' in data:
-                card_dict.append('even')
-            if 'odd' in data:
-                card_dict.append('odd')
-            if 'is_royal' in data:
-                card_dict.append('is_royal')
-            if 'color' in data:
-                if 'R),' in data:
-                    card_dict.append('color')
-                    card_dict.append('R')
-                else:
-                    card_dict.append('color :B')
-                    card_dict.append('B')
-            if 'suit' in data:
-                if 'H),' in data:
-                    card_dict.append('suit')
-                    card_dict.append('H')
-                elif 'C),' in data:
-                    card_dict.append('suit :C')
-                    card_dict.append('C')
-                elif 'D),' in data:
-                    card_dict.append('suit :D')
-                    card_dict.append('D')
-                elif 'S),' in data:
-                    card_dict.append('suit :S')
-                    card_dict.append('S')
-            if 'greater' in data:
-                card_dict.append('greater')
-            if 'less' in data:
-                card_dict.append('less')
-            print "hello",card_dict
-            best_dict= {}
-            for  card in range(0,14):
-                extr = extraction(self.hand[card])
-                choice_score = 0
-                for j in range(len(card_dict)):
-                    if card_dict[j] in extr.keys() and extr[card_dict[j]] == 1:
-                        choice_score = choice_score + 1
-                    if card_dict[j] == 'greater':
-                        if(greater(previous, self.hand[card])):
-                            choice_score = choice_score + 1
-                    if card_dict[j] == 'less':
-                        if(less(previous, self.hand[card])):
-                            choice_score = choice_score + 1
-                best_dict[self.hand[card]] = choice_score
-
-        elif 'andf' in data:
-            print "hi"
-        elif 'iff' in data:
-            print "hold"
-        card = 0
-        print self.hand
-        print best_dict
-        print max(best_dict.values())
-        for k,v in best_dict.items():
-            if v==max(best_dict.values()):
-                print k
-                break
-        #self.hand.pop(k)
-        #print self.hand
-        #return scientist(cards, self.hand, game_ended)
-        p=parse(rule).evaluate(cards)
-        print p
-        #t=Tree(p.root)
-        #print t
-       # print t.evaluate(cards)
+        print "player play"
+        return scientist(hypothesis,correct[(len(correct)-3):len(correct)], self.hand, game_ended)
 
 
 class Adversary(object):
@@ -159,37 +66,69 @@ adversary2 = Adversary()
 adversary3 = Adversary()
 
 # Set a rule for testing
-rule = "if(is_royal(current), False, True)"
+rule = "if(greater(prev,current),True, False)"
 #setRule(rule)
 
 # The three cards that adhere to the rule
-cards = ["2D", "4H", "6H"]
-print cards[2]
+cards = ["4S", "6C", "8S"]
+for i in cards:
+    correct.append(i)
 """
 In each round scientist is called and you need to return a card or rule.
 The cards passed to scientist are the last 3 cards played.
 Use these to update your board state.
 """
-'''
+hypothesis=perform(cards)
+flag=0
 for round_num in range(14):
     # Each player plays a card or guesses a rule
     try:
         #Player 1 plays
-        player_card_rule = player.play()
-        if is_card(player_card_rule):
+        player_card_rule = player.play(hypothesis,cards) 
+        if is_card(player_card_rule) and flag<2:
             del cards[0]
             cards.append(player_card_rule)
-        else :
+            test=correct[(len(correct)-2):len(correct)]
+            test.append(player_card_rule)
+            p=parse(rule).evaluate(test)
+            print "evaluate",p
+            if p==True or p=='True':
+                flag+=1
+                print "player correct",correct, "flag",flag
+                correct.append(player_card_rule)
+            else:
+                flag=0
+                wrong.append(player_card_rule)
+                print "player wrong",wrong
+                hypothesis=prune(hypothesis,correct)
+                print "adv",hypothesis
+        else:
+            print "OUR RULE:", "if(",hypothesis,",True)"
             raise Exception('')
-        
 
         #Adversary 1 plays
         ad1_card_rule = adversary1.play()
         if is_card(ad1_card_rule):
             del cards[0]
             cards.append(ad1_card_rule)
+            test=correct[(len(correct)-2):len(correct)]
+            test.append(ad1_card_rule)
+            p=parse(rule).evaluate(test)
+            print "ad1",ad1_card_rule
+            print "evaluate",p
+            if p==True or p=='True':
+                correct.append(ad1_card_rule)
+                print "adversary1",correct
+                hypothesis=prune(hypothesis,correct)
+                print "ad1 prune",hypothesis
+            else:
+                wrong.append(ad1_card_rule)
+                print "ad1 wrong",wrong
+                #hypothesis=prune(hypothesis,correct)
             #print cards
         else:
+            print "saale 1 ko mil gya"
+            print "bechara hamara rule","if(",hypothesis,",True)"
             raise Exception('')
 
         #Adversary 2 plays
@@ -197,8 +136,25 @@ for round_num in range(14):
         if is_card(ad2_card_rule):
             del cards[0]
             cards.append(ad2_card_rule)
+            test=correct[(len(correct)-2):len(correct)]
+            test.append(ad2_card_rule)
+            p=parse(rule).evaluate(test)
+            print "ad2",ad2_card_rule
+            print "evaluate",p
+
+            if p==True or p=='True':
+                correct.append(ad2_card_rule)
+                print "adversary1",correct
+                hypothesis=prune(hypothesis,correct)
+                print "ad1 prune",hypothesis
+               
+            else:
+                wrong.append(ad2_card_rule)
+                
             #print cards
         else:
+            print "saale 2 ko mil gya"
+            print "bechara hamara rule","if(",hypothesis,",True)"
             raise Exception('')
 
         #Adversary 3 plays
@@ -206,16 +162,31 @@ for round_num in range(14):
         if is_card(ad3_card_rule):
             del cards[0]
             cards.append(ad3_card_rule)
+            test=correct[(len(correct)-2):len(correct)]
+            test.append(ad3_card_rule)
+            p=parse(rule).evaluate(test)
+            print "ad3",ad3_card_rule
+            if p==True or p=='True':
+                correct.append(ad3_card_rule)
+                hypothesis=prune(hypothesis,correct)
+                print "ad3 prune",hypothesis
+                print "evaluate",p
+
+            else:
+                wrong.append(ad3_card_rule)
+                
             #print cards
         else:
+            print "saale 3 ko mil gya"
+            print "bechara hamara rule","if(",hypothesis,",True)"
             raise Exception('')
 
     except:
         game_ended = True
         break
-'''
+
 # Everyone has to guess a rule
-rule_player = player.play(cards)
+#rule_player = player.play(cards)
 
 # Check if the guessed rule is correct and print the score
 #score(player)
--- rule.py
+++ rule.py
@@ -1,59 +1,153 @@
-from new_eleusis import *;
-            
-def perform():
-    cards=['4H','2H','2D']
-    #rule= If (equal(suit(current), suit(prev)), greater(value(current), value(prev)), True)
+from new_eleusis import *
+from Main import *
+from random import randint
+def generate_random_cards():
+    values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suits = ["S", "H", "D", "C"]
+    return values[randint(0, len(values)-1)] + suits[randint(0, len(suits)-1)]
+
+def scientist(data, cards, hand, game_ended):
+    card_dict = []
+    current_hand = [hand[0], cards[2], cards[1]]
+    current  = hand[0]
+    previous = cards[2] #replace with card from corrent list later
+    previous2 = cards[1]
+    print "data",data
+    #data = "orf(even(current),equal(color(current),R),greater(prev,current)) "
+    if 'orf' in data:
+        data = data.split('orf(')[1]
+        data = data.rsplit(')', 1)[0]
+        #data = data.split("),")
+        if 'even' in data:
+            card_dict.append('even')
+        if 'odd' in data:
+            card_dict.append('odd')
+        if 'is_royal' in data:
+            card_dict.append('is_royal')
+        if 'color' in data:
+            if 'R),' in data:
+                #card_dict.append('color')
+                card_dict.append('R')
+            else:
+                #card_dict.append('color :B')
+                card_dict.append('B')
+        if 'suit' in data:
+            if 'H),' in data:
+                card_dict.append('suit')
+                card_dict.append('H')
+            elif 'C),' in data:
+                card_dict.append('suit :C')
+                card_dict.append('C')
+            elif 'D),' in data:
+                card_dict.append('suit :D')
+                card_dict.append('D')
+            elif 'S),' in data:
+                card_dict.append('suit :S')
+                card_dict.append('S')
+        if 'greater' in data:
+            card_dict.append('greater')
+        if 'less' in data:
+            card_dict.append('less')
+        print "hello",card_dict
+        best_dict= {}
+        for card in range(0,14):
+            extr = extraction(hand[card])
+            choice_score = 0
+            for j in range(len(card_dict)):
+                if card_dict[j] in extr.keys() and extr[card_dict[j]] == 1:
+                    choice_score = choice_score + 1
+                if card_dict[j] == 'greater':
+                    if(greater(previous, hand[card])):
+                        choice_score = choice_score + 1
+                if card_dict[j] == 'less':
+                    if(less(previous, hand[card])):
+                        choice_score = choice_score + 1
+            #print choice_score
+            best_dict[hand[card]] = choice_score
+
+    elif 'andf' in data:
+        print "hi"
+    elif 'iff' in data:
+        print "hold"
+    card = 0
+    print "hand",hand
+    print "best",best_dict
+    print max(best_dict.values())
+    for k,v in best_dict.items():
+        if v==max(best_dict.values()):
+            print "card gen",k
+            selected=k
+            break
+    print "after"
+    hand.remove(selected)
+    print "rem",hand
+    r = [generate_random_cards() for i in range(14)]
+    print "r",r
+    hand.append(r[0])
+    print "ad",hand
+    return selected
+    #break
+    #self.hand.pop(k)
+    #print self.hand
+    #return scientist(cards, self.hand, game_ended)
+    #p=parse(rule).evaluate(cards)
+    #return p
+    #t=Tree(p.root)
+    #print t
+
+def perform(cards):
+    #rule= If (equal(suit(current), suit(prev)), greater(value(current), value(prev)), True)
     prev2=cards[0]
     prev1=cards[1]
     current=cards[2]
     list=[]
-    str1="or("
+    str1="orf("
     #all even
     if even(prev2)==True and even(prev1)==True and even(current)==True:
         #all are even
         print "all even"
         list.append("even(current)")
-        
+
     #all are odd
     if odd(prev2)==True and odd(prev1)==True and odd(current)==True:
         #all are odd
         print "all odd"
         list.append("odd(current)")
-        
+
     #all are royal
     if is_royal(prev2)==True and is_royal(prev1)==True and is_royal(current)==True:
         #all are royal
         print "all royal"
         list.append("is_royal(current)")
-        
+
     #all have same color
     if equal(color(prev2),color(prev1)) and equal(color(prev1),color(current)):
         #all are red
         print "all same color"
         if equal(color(prev2),'R')==True:
             print "all red"
-            list.append("equal(color(current),'R')")
+            list.append("equal(color(current),R)")
         else:
             print "all black"
-            list.append("equal(color(current),'B')")
-    
+            list.append("equal(color(current),B)")
+
     #all belong to the same suit
     if equal(suit(prev2),suit(prev1)) and equal(suit(prev1),suit(current)):
         print "same suit"
         if equal(suit(current),'C'):
             print "all clubs"
-            list.append("equal(suit(current),'C')")
+            list.append("equal(suit(current),C)")
         elif equal(suit(current),'H'):
             print "all hearts"
-            list.append("equal(suit(current),'H')")
+            list.append("equal(suit(current),H)")
         elif equal(suit(current),'S'):
             print "all spades"
-            list.append("equal(suit(current),'S')")
+            list.append("equal(suit(current),S)")
 
         else:
             print "all diamonds"
-            list.append("equal(suit(current),'D')")
-        
+            list.append("equal(suit(current),D)")
+
     if less(prev2,prev1)==True:
         if less(prev1,current)==True:
             #alternate less
@@ -75,7 +169,5 @@ def perform():
         str1+=i+","
     str1=str1[:len(str1)-1]
     str1+=")"
-    print str1
-        
-perform()
+    return str1
 
