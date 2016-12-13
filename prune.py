f550c65852b66d80de189cd813311cc37b870f71
Srishty12107 <Srishty12107@iiitd.ac.in>
2016-12-12 22:06:55 -0500

    prune final

--- prune.py
+++ prune.py
@@ -1,8 +1,8 @@
 import re
 import new_eleusis
 import Main
-#rule="orf(equal(even(previous2),even(current)),equal(color(current),B),greater(prev,current),notf(is_royal(current)))"
-#correct=['4H','4S','3S','3H']
+#rule="orf(even(current),equal(color(current),B),equal(suit(current),S),less(value(prev),value(current)),notf(is_royal(current)))"
+#correct=["4S", "6S", "8S","2H"]
 
 ##def func(p):
 ##    for k,v in p.iteritems():
@@ -11,9 +11,10 @@ import Main
 def prune(rule,correct): #probability of current
     n=[]
     n=re.findall("[\w']+",rule)
-    print n
     #print n
-    d=correct[(len(correct)-3):len(correct)]
+    #print n
+    d=correct[len(correct)-3:len(correct)]
+    #print d
     f=[]
     for i in range(0,len(d)):  #for general rule
         c=Main.extraction(d[i])
@@ -27,33 +28,35 @@ def prune(rule,correct): #probability of current
 
     for i in range(0, len(n)):  #for greater,equal,less
             if(n[i]=='equal'):#k-m omk mk                                                                                                                                                      1
-                    if(new_eleusis.equal(d[1],d[2])):
+                    if(new_eleusis.equal(d[2],d[1]) and new_eleusis.equal(d[0],d[1])):
+                        #print"equal"
                         f.append('equal')
             if(n[i]=='greater'):
-                print 'hello'
-                print new_eleusis.value(d[0])
-                if(new_eleusis.value(d[0])>new_eleusis.value(d[1]) and new_eleusis.value(d[1])>new_eleusis.value(d[2])):
-                    print 'hello'
+                #print 'hello'
+                #print new_eleusis.value(d[0])
+                if(new_eleusis.value(d[len(d)-3])>new_eleusis.value(d[len(d)-2]) and new_eleusis.value(d[len(d)-2])>new_eleusis.value(d[len(d)-1])):
+                    #print 'hello'
                     f.append('value_greater')
                 else:
                     f.append('greater')
             if(n[i]=='less'):
-                if(new_eleusis.value(d[0])<new_eleusis.value(d[1]) and new_eleusis.value(d[1])<new_eleusis.value(d[2])):
+                if(new_eleusis.value(d[len(d)-3])< new_eleusis.value(d[len(d)-2]) and new_eleusis.value(d[len(d)-2])< new_eleusis.value(d[len(d)-1])):
+                    #print "hi" # and new_eleusis.value(d[len(d)-2])<new_eleusis.value(d[len(d)-1])):
                     f.append('value_less')
                 else:                    #if(new_eleusis.less(d[1],d[2])):
                     f.append('less')
 
-    print "l"
-    print n
+    #print "l"
+    #print n
     count=0
     for i in range(0,len(n)):
         if(n[i]=='notf'):
            for j in range(0,len(d)):
                c1=Main.extraction(d[j])
-               print c1
+               #print c1
                if(c1['royal']==1):
                    count=count+1
-                   print "hi"
+                   #print "hi"
                    break
            if(count>0):
                 break
@@ -61,28 +64,45 @@ def prune(rule,correct): #probability of current
               f.append('not_royal')
         
                     
-            
-           
+    for i in range(0,len(n)):
+        if(n[i]=='less'):
+            for j in range(0,len(f)):
+                if(f[j]=='value_less' or f[j]=='less'):
+                    for k in range(0,len(d)):
+                        #print "hi1"
+                        if(new_eleusis.value(d[len(d)-3])- new_eleusis.value(d[len(d)-2])==-1 and new_eleusis.value(d[len(d)-2])- new_eleusis.value(d[len(d)-1])==-1):
+                            #print "hi2"
+                            f.append('plus1')
+
+    for i in range(0,len(n)):
+        if(n[i]=='greater'):
+            for j in range(0,len(f)):
+                if(f[j]=='value_greater' or f[j]=='greater'):
+                    for k in range(0,len(d)):
+                        if(new_eleusis.value(d[len(d)-3])- new_eleusis.value(d[len(d)-2])== 1 and new_eleusis.value(d[len(d)-2])- new_eleusis.value(d[len(d)-1])==1):
+                            #print "hi2"
+                            f.append('minus1')
+                              
     final=[]
     #print final
     for x in f:#pruning is_color
                 if x not in final:
                         final.append(x)
 
-    print "pruned",final                    
+    #print "pruned",final                    
     str1="orf("
     list=[]
     for s in range(0,len(final)):
-        if(final[s]=='R'):
-            list.append("equal(color(current),R)")
-        if(final[s]=='B'):
-            list.append("equal(color(current),B)")
         if(final[s]=='even'):
             list.append("even(current)")
         if(final[s]=='odd'):
             list.append("odd(current)")
         if(final[s]=='royal'):
             list.append("is_royal(current)")
+        if(final[s]=='R'):
+            list.append("equal(color(current),R)")
+        if(final[s]=='B'):
+            list.append("equal(color(current),B)")
         if(final[s]=='C'):
             list.append("equal(suit(current),C)")
         if(final[s]=='D'):
@@ -99,8 +119,13 @@ def prune(rule,correct): #probability of current
             list.append("greater(value(prev),value(current))")
         if(final[s]=='greater'):
             list.append("greater(prev,current)")
+        if(final[s]=='plus1'):
+            list.append("plus1(value(prev),value(current))")
+        if(final[s]=='minus1'):
+            list.append("minus1(value(prev),value(current))")
         if(final[s]=='not_royal'):
             list.append("notf(is_royal(current))")
+      
 
             
     for i in list:
@@ -113,24 +138,6 @@ def prune(rule,correct): #probability of current
                        
 
                     
-##     c1=Main.extraction(correct[0])
-##        c2=Main.extraction(correct[1])
-##        c3=Main.extraction(correct[2])
-##        d3=Main.comparator(c1,c2,c3)
-##        i=3
-##        while(i<len(correct)):
-##                d2=Main.comparator(c1,c2,c3)
-##                d1=d2.keys()
-##                for i1 in range(0,len(d1)):
-##                        for j in range(0,len(h)):
-##                                if(d1[i1]==h[j]):
-##                                        if(d2[d1[i1]]==1):
-##                                                q.append(d1[i1])
-##                c2=c1
-##                c3=c2
-##                c1=Main.extraction(correct[i])
-##                i=i+1
-##                if(i==len(correct)):
-##                        break                   
+             
     return str1
 #print prune(rule,correct)
